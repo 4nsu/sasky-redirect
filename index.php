7ff90110a27f0af7a9bc8e703af991a115f3e4b7
ansu <annastiina.koivu@gmail.com>
2024-04-23 15:15:05 +0300

    muodostetaan sivupohja

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+// Pagestatus määrittelee mikä sivu tulostetaan.
+//  0 = etusivu
+// -1 = virheellinen tunniste
+// -2 = tietokantavirhe
+$pagestatus = 0;
+
+// Tallennetaan perusosoite muuttujaan
+$baseurl = "https://neutroni.hayo.fi/~akoivu/redirect/";
+
 // Määritellään yhteys-muuttujat.
 // Tietokannan nimi, käyttäjä ja salasana, haetaan palvelimen ympäristömuuttujista.
 $dsn = "mysql:host=localhost;dbname={$_SERVER['DB_DATABASE']};charset=utf8mb4";
@@ -53,20 +62,79 @@ if (isset($_GET["hash"])) {
 
             // Kannassa ei ole hash-muuttujaa vastaavaa tunnistetta,
             // tulostetaan virheilmoitus.
-            echo "Väärä tunniste :(";
+            $pagestatus = -1;
 
         }
 
     } catch (PDOException $e) {
-        echo $e->getMessage();
+        // Virhe avaamisessa, tulostetaan virheilmoitus.
+        $pagestatus = -2;
+        $error = $e->getMessage();
     }
-
-} else {
-
-    // hash-parametrille ei löytynyt arvoa,
-    // tulostetaan käyttäjälle esittelyteksti.
-    echo "Tämä on osoitteiden lyhentäjä.<br>Odota maltilla, tänne tulee tulevaisuudessa lisää toiminnallisuutta.";
 
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lyhentäjä</title>
+</head>
+<body>
+    <div class='page'>
+
+        <header>
+            <h1>Lyhentäjä</h1>
+            <div>ällistyttävä osoitelyhentäjä</div>
+        </header>
+
+        <main>
+            <?php 
+                if($pagestatus == 0) { 
+            ?>
+                <div class='form'>
+                    <p>Tällä palvelulla voit lyhentää pitkän osoitteen lyhyeksi. Syötä alla olevaan kenttään pitkä osoite ja paina nappia, saat käyttöösi lyhytosoitteen, jota voit jakaa eteenpäin.</p>
+                    <form action='' method='POST'>
+                        <label for='url'>Syötä lyhennettävä osoite</label>
+                        <div class='url'>
+                            <input type='text' name='url' placeholder='tosi pitkä osoite'>
+                            <input type='submit' name='shorten' value='lyhennä'>
+                        </div>
+                    </form>
+                </div>
+            <?php 
+                }
+
+                if($pagestatus == -1) { 
+            ?>
+                <div class='error'>
+                    <h2>HUPSISTA!</h2>
+                    <p>Näyttää siltä, että lyhytosoitetta ei löytynyt. Ole hyvä ja tarkista antamasi osoite.</p>
+                    <p>Voit tehdä <a href="<?=$baseurl?>">tällä palvelulla</a> oman lyhytosoitteen.</p>
+                </div>
+            <?php 
+                }
+
+                if($pagestatus == -2) { 
+            ?>
+                <div class='error'>
+                    <h2>NYT KÄVI HASSUSTI!</h2>
+                    <p>Nostamme käden ylös virheen merkiksi, palvelimellamme on pientä hässäkkää. Ole hyvä ja kokeile myöhemmin uudelleen.</p>
+                    <p>(virheilmoitus: <?=$error?>)</p>
+                </div>
+            <?php 
+                }
+            ?>
+        </main>
+
+        <footer>
+            <hr>
+            &copy; Kurpitsa Solutions
+        </footer>
+
+    </div>
+</body>
+</html>

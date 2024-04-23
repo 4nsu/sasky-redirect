0120e287fa951c9aea4604821130b3ccdd781f82
ansu <annastiina.koivu@gmail.com>
2024-04-23 16:25:09 +0300

    lisää ulkoasutyylit

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ if (isset($_GET["hash"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='styles.css' rel='stylesheet'>
     <title>Lyhentäjä</title>
 </head>
 <body>
